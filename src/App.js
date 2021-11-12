b4e994447e6087034040534b3b470b97bc2f1b8a
danil <minakonn9shkoni@gmail.com>
2021-11-12 11:57:48 +0300

    Add ArrayMap

--- src/App.js
+++ src/App.js
@@ -1,45 +1,73 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { Component } from "react";
 import Aloha from "./components/Aloha";
 
 // function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+// return (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//       </a>
+//     </header>
+//   </div>
+// );
 
-  // return <h1>Hi! JSX</h1>;
-  // return React.createElement('h1',null,'Hi! JSX');
-  
-  class App extends Component(){
-    constructor(props){
-      super(props);
-      this.state = {
-        fname:'Elon',
-        sname:'Musk'
-      }
-    }
-    render(){
-      const {fname} = this.state;
-      return <>
-      <Aloha name={fname}
-      </>
-    }
+// return <h1>Hi! JSX</h1>;
+// return React.createElement('h1',null,'Hi! JSX');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          id: 1,
+          fname: "Elon",
+        },
+        {
+          id: 2,
+          fname: "Rob",
+        },
+        {
+          id: 3,
+          fname: "Elly",
+        },
+        {
+          id: 4,
+          fname: "Muck",
+        },
+        {
+          id: 5,
+          fname: "Tim",
+        },
+        {
+          id: 6,
+          fname: "Le",
+        },
+      ],
+    };
   }
+  render() {
+    const { users } = this.state;
+    return (
+      <>
+        {users.map(({ fname }) => (
+          <Aloha name={fname} />
+        ))}
+      </>
+    );
+  }
+}
 
 export default App;
