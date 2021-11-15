24a6128e646fa5f0d316bba25181982d045d66f5
danil <minakonn9shkoni@gmail.com>
2021-11-15 09:20:52 +0300

    Add sort by name

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     super(props);
     this.state = {
       isDirection: true,
-      isAlphabet: true,
+      isAlpha: true,
       users: [
         {
           id: 1,
@@ -60,24 +60,39 @@ class App extends Component {
       ],
     };
   }
+
+  sortUsersAlpha = () => {
+    const { users, isAlpha } = this.state;
+    const sortUsersAlpha = JSON.parse(JSON.stringify(users));
+    sortUsersAlpha.sort(function isAlpha(a,b) {
+      if(a.fname>b.fname){
+        return 1;
+      }
+      if(a.fname<b.fname){
+        return -1;
+      }
+      return 0;
+    })
+    this.setState({
+      isAlpha: !isAlpha,
+      users: sortUsersAlpha,
+    });
+  };
+
   sortUsers = () => {
-    const { users, isDirection, isAlphabet } = this.state;
+    const { users, isDirection } = this.state;
     const sortUsers = JSON.parse(JSON.stringify(users));
     // const sortUsers = [...users];
     sortUsers.sort((prev, next) =>
       isDirection ? next.id - prev.id : prev.id - next.id
     );
-    sortUsers.sort((a, b) =>
-      isAlphabet ? a.fname > b.fname : b.fname < a.fname
-    );
     this.setState({
-      isAlphabet: !isAlphabet,
       isDirection: !isDirection,
       users: sortUsers,
     });
   };
   render() {
-    const { users, isDirection, isAlphabet } = this.state;
+    const { users, isDirection, isAlpha } = this.state;
     return (
       <>
         <p>
@@ -85,8 +100,8 @@ class App extends Component {
           <button onClick={this.sortUsers}>SORT by id</button>
         </p>
         <p>
-          {isAlphabet ? "straight" : "reverse"}
-          <button onClick={this.sortUsers}>SORT by Name</button>
+          {isAlpha ? "straight" : "reverse"}
+          <button onClick={this.sortUsersAlpha}>SORT by Name</button>
         </p>
         {users.map(({ id, fname }) => (
           <Aloha name={fname} key={id} id={id} />
